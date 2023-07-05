ec57e7d8af9093db7c91f490cd9d445bca2dd72d
FMonteg <fabien.montegut@gmail.com>
2023-07-05 18:11:03 +0200

    Reprise de la classe Contact_Smogon -> à compléter

--- contact_smogon.py
+++ contact_smogon.py
@@ -1,13 +1,7 @@
-'''
-File written by Alan on Smogon forum
-'''
-
 import requests
 import os
 import shutil
 import pandas as pd
-import tier_lookup
-import time
 
 '''
 https://www.smogon.com/stats/2018-04/gen7pususpecttest-1500.txt
@@ -30,74 +24,107 @@ monotypes=['monodark','monobug','monodragon','monofighting','monoflying',
 
 class Contact_Smogon():
 
-    def __init__(self,year,mm,gen,tier,rating):
-        self.yyyy=str(year) #needs to be 4 digit year
-        self.mm=str(mm) #needs to be 2 digit month, ie 03 for March
-        self.rating=rating
-        self.tier=tier
-        self.path=None
-        self.gen=gen
+    def __init__(self, **kwargs):
+        
+        self.rating = kwargs.get('rating','0')
+        self.gen = kwargs.get('gen','gen7')
+        self.tier = kwargs.get('tier','ou')
+        self.path = kwargs.get('path',os.getcwd())
+        
+        self.possible_rating = [0,1500,1630,1760]
+        self.possible_gen = ['gen8','gen7','gen6','gen5','gen4','gen3','gen2','gen1']
+        self.possible_tier = ['ou','ubers','1v1','pu','nu','lc','uu','ru','customgame','2v2doubles',
+                              'doubles','anythinggoes','almostanyability','balancedhackmons',
+                              'battlefactory','battlespotsingles','battlespotdoubles','battlespotspecial9',
+                              'bssfactory','camomons','cap','challengecup1v1','doublescustomgame',
+                              'doublesou','doublesubers','doublesuu','hackmonscup','linked','mixandmega',
+                              'natureswap','stabmons','ultrakalosclassic','vcg2018','vgc2014',
+                              'vgc2015','vgc2016','vgc2017','vgc2019','vgc2020']
 
-    #create temporary folder to store txt for parse
-    def _make_temp(self):
-        path = os.getcwd()
-        folder_name = r'\temp_folder'
-        os.mkdir(path+folder_name)
-        global _temp_folder
-        _temp_folder = path+folder_name
-        print("Temporary folder successfully created.")
-        return _temp_folder
+        pass
+    
+    def get_possible_rating(self):
+        return self.possible_rating
+    
+    def get_possible_gen(self):
+        return self.possible_gen
+    
+    def get_possible_tier(self):
+        return self.possible_tier
+    
+    def set_rating(self, rating):
+        if rating in self.possible_rating:
+            self.rating = rating
+        else:
+            print("HALT AND CATCH FIRE (invalid rating)")
+           
+    def set_gen(self, gen):
+        if gen in self.possible_gen:
+            self.gen = gen
+        else:
+            print("HALT AND CATCH FIRE (invalid gen)")
+            
+    def set_tier(self, tier):
+        if tier in self.possible_tier:
+            self.tier = tier
+        else:
+            print("HALT AND CATCH FIRE (invalid tier)")
+    
+    
 
-    #remove temporary folder
-    def _clear_temp(self):
-        temp_path = os.fsencode(_temp_folder)
-        shutil.rmtree(temp_path)
-        print("Temporary files have been removed from {}.".format(_temp_folder))
-        return
-
-    #find the stats for the tier of interest, will be parsed later
-    def find_stats(self,urls):#,gen,tier,rating):
-        # rating_list=['0','1500','1630','1760'] #list of possible ratings
-        # if rating not in rating_list: #throw error if invalid rating.
-        #     raise ValueError("Invalid rating input. Rating value must be 1 of ["
-        #                      "0, 1500, 1630, 1760]")
-        #
-        # #set class variables
-        # self.gen = gen
-        # self.tier=tier
-        # self.rating=rating
-        #
-        # #url to get perform the request
-        # urls=d.generate_urls()
-
-        #do the request. get the obj and the text. ave text to file and
-        # begin edit process. may abstract away to outside function in future.
+    def make_folder(self, folder_name):
+        folder_path = self.path+folder_name
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
+        print("Folder {} succesfully created".format(folder_name)
+        return folder_path
+       
+              
+    def request_data(self, **kwargs):
+              
+        dates = kwargs.get('dates', ['2020-05']) #FLAG Temporaire
+              
+        #Step 1 : créer les URLs FLAG
+              
+              
+        #Step 2 : réclamer les données à Smogon
         for url in urls:
             r=requests.get(url)
+            
+            #On vérifie que le fichier existe
+            if r.status_code == 404:
+                continue
 
+            #On récupère les métadonnées
             page = r.text
-
             src = url.split('/')
             src=src[4:]
             print(src)
+            
             #save files based on unique values.
-            page_path=(r'C:\Dev\usage_data_0.0.1\scripts\temp_folder\rawstats_'
-                      r'{}_{}').format(src[0],src[1])
+            folder_path = self.make_folder('\raw_data')
+            page_path=(folder_path + r'\rawstats_{}_{}').format(src[0],src[1])
             print(page_path)
+              
             #save file
             with open(page_path,"w") as f:
                 f.write(page)
                 f.close()
+              
+              
+            #FLAG gérer l'après cette ligne (et donc les deux fonctions qui vont avec)
 
             #read file in with readlines(), remove first 5 lists of garbage
             # elements. begin text formatting.
             fobj = open(page_path)
             data_list=Contact_Smogon._remove_formatting(self,fobj)
             df = Contact_Smogon.create_data_structure(self,data_list=data_list)
-            time.sleep(5)
+              
+              
+        return
+    
 
-            return df
-
+    
     #Function to: create data structure
     def create_data_structure(self,data_list):
         #key pairs, mirroring the headers in the raw text
@@ -115,24 +142,15 @@ class Contact_Smogon():
 
         #make dataframe to save and use for plotting
         df = pd.DataFrame(dict_list)
-        df.to_csv(r'C:\Dev\usage_data_0.0.1\data\{}_{}_{}{}_{}.csv'.format(
+        path = os.getcwd()
+        df.to_csv(path + r'\{}_{}_{}{}_{}.csv'.format(
             self.yyyy,self.mm,self.gen,self.tier,self.rating),index=False)
 
         return df
 
 
-    # def clean_stats(self, page_path):
-    #     with open(page_path,'w') as s:
-    #         lines = s.readlines()
-    #         for line in lines:
-    #             print(line)
-    #
-    #     return
-        # headers=['rank','pokemon','usage_pct','raw_usage','raw_pct','real',
-        #          'real_pct']
-
     def _remove_formatting(self,page):
-        #create blank list to be filled and retunred
+        #create blank list to be filled and returned
         outlist=list()
 
         #read lines of file with .readlines() and truncate the first 6 lines of
